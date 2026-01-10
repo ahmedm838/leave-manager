aaaf311b60a80b8691e2378d924a4177ecf2f1e2
AhmedMoustafa88 <a.moustafa.m88@gmail.com>
2026-01-10 16:30:37 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { initTheme } from './lib/theme'
@@ -10,8 +10,12 @@ initTheme()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    {/*
+      HashRouter avoids 404s on static hosts (GitHub Pages, cPanel, GoDaddy, etc.)
+      when users refresh deep links like /login or /app/dashboard.
+    */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
