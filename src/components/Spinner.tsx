63bf4a39e779293dfef11d5e4b16813f574196e0
AhmedMoustafa88 <a.moustafa.m88@gmail.com>
2026-01-11 10:57:02 +0200

    update

--- src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-export function Spinner() {
-  return (
-    <div
-      className="inline-block h-5 w-5 animate-spin rounded-full border-2 border-slate-300 dark:border-slate-700 border-t-slate-900 dark:border-t-slate-100"
-      aria-label="Loading"
-    />
-  )
-}
