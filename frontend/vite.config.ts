09b4d6456c448001d06abadddafe50010770faf5
Ahmed  Moustafa <161363063+ahmedm838@users.noreply.github.com>
2026-01-11 12:02:55 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,7 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Use relative base to avoid %BASE_URL% issues on GitHub Pages.
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: "/leave-manager/",
 });
